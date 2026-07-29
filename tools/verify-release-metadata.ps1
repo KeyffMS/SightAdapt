1e295b17224d3df5b5d3a4d4449bfd6cdaca89be
KeyffMS <124252104+KeyffMS@users.noreply.github.com>
2026-07-29 15:02:50 +0200

    Select project metadata with XPath (#83)

--- tools/verify-release-metadata.ps1
+++ tools/verify-release-metadata.ps1
@@ -56,14 +56,7 @@ if ([string]$globalJson.sdk.version -ne $sdkVersion -or
 }
 
 [xml]$project = Get-Content (Join-Path $root 'src/SightAdapt/SightAdapt.csproj')
-$metadata = @(
-    $project.Project.ItemGroup |
-        ForEach-Object {
-            if ($null -ne $_.AssemblyMetadata) {
-                @($_.AssemblyMetadata)
-            }
-        }
-)
+$metadata = @($project.SelectNodes('/Project/ItemGroup/AssemblyMetadata'))
 if (-not ($metadata | Where-Object {
     $_.Include -eq 'Milestone' -and
     $_.Value -eq '$(SightAdaptMilestone)'
