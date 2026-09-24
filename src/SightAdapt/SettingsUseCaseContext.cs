6ed14d9e8d7b4be6b9393776e145a32e9cfd7108
KeyffMS <124252104+KeyffMS@users.noreply.github.com>
2026-09-24 15:02:31 +0200

    Scope settings change origin to each use-case context
    Addresses #116.
    
    Signed-off-by: KeyffMS <124252104+KeyffMS@users.noreply.github.com>

--- src/SightAdapt/SettingsUseCaseContext.cs
+++ src/SightAdapt/SettingsUseCaseContext.cs
@@ -3,6 +3,7 @@ namespace SightAdapt;
 internal sealed class SettingsUseCaseContext
 {
     private readonly SettingsCoordinator _coordinator;
+    private readonly object _changeOrigin = new();
 
     public SettingsUseCaseContext(
         SettingsCoordinator coordinator)
@@ -17,17 +18,30 @@ internal sealed class SettingsUseCaseContext
     public string SettingsPath =>
         _coordinator.SettingsPath;
 
-    public event EventHandler? Changed
+    public event EventHandler<SettingsChangedEventArgs>? Changed
     {
         add => _coordinator.Changed += value;
         remove => _coordinator.Changed -= value;
     }
 
+    public bool IsLocalChange(
+        SettingsChangedEventArgs eventArgs)
+    {
+        ArgumentNullException.ThrowIfNull(eventArgs);
+        return ReferenceEquals(
+            eventArgs.Origin,
+            _changeOrigin);
+    }
+
     public SettingsCommitResult Commit(
         Action<SightAdaptSettings> mutation) =>
-        _coordinator.Commit(mutation);
+        _coordinator.Commit(
+            mutation,
+            _changeOrigin);
 
     public SettingsCommitResult<T> Commit<T>(
         Func<SightAdaptSettings, T> mutation) =>
-        _coordinator.Commit(mutation);
+        _coordinator.Commit(
+            mutation,
+            _changeOrigin);
 }
